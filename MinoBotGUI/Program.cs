2d2d9632f1bf7c79e007032466c9d9003bbc30ff
KSean222 <44050761+KSean222@users.noreply.github.com>
2020-02-07 19:39:50 +1000

    Fixed logging bug

--- MinoBotGUI/Program.cs
+++ MinoBotGUI/Program.cs
@@ -107,7 +107,7 @@ namespace MinoBotGUI
                         Console.WriteLine($"Move {movesMade}.");
                         Console.WriteLine($"Thought for {elapsed}ms (avg: {totalThinkMS / (double) movesMade})");
                         Console.WriteLine($"{thinks} thinks (avg: {totalThinks / (double) movesMade})");
-                        Console.WriteLine($"{stopwatch.ElapsedMilliseconds / (double) thinks } ms per think (avg: {totalThinkMS / (double) totalThinks})");
+                        Console.WriteLine($"{elapsed / (double) thinks } ms per think (avg: {totalThinkMS / (double) totalThinks})");
                         Console.WriteLine($"Depth: {bot.maxDepth} (avg: {totalDepth / (double) movesMade}).");
                         Console.WriteLine($"Nodes: {nodes} (avg: {totalNodes / (double )movesMade})");
                         Console.WriteLine("Selected node has:");
